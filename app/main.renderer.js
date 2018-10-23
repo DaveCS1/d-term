3092719344d65f591b60d29331e7a82fc4c19695
akasarto <github@thiagoschneider.com>
2018-10-23 00:05:03 -0300

    Encapsulating terminal instances

--- app/main.renderer.js
+++ app/main.renderer.js
@@ -1,43 +1,14 @@
-const pty = require('node-pty');
-const term = require('xterm').Terminal;
-const { remote, ipcRenderer } = require('electron');
-const { fit } = require('xterm/lib/addons/fit/fit');
+const { remote } = require('electron');
 const currentWindow = remote.getCurrentWindow();
-
-const xterm = new term();
-
-const ptyProcess = pty.spawn('powershell.exe', [], {
-    name: 'xterm-color',
-    cwd: process.cwd(),
-    env: process.env,
-    cols: 80,
-    rows: 30
-});
-
-xterm.open($('#terminal')[0]);
-
-xterm.on('data', (data) => {
-    ptyProcess.write(data);
-});
-
-ptyProcess.on('data', function (data) {
-    xterm.write(data);
-});
-
-ptyProcess.on('exit', function (data) {
-    currentWindow.close();
-});
+const instance = new term({}, $('#terminal')[0]);
+const term = require('./xterm.instance');
 
 currentWindow.on('resize', () => {
     fit(xterm);
 });
 
-currentWindow.on('close', function () {
-    ipcRenderer.send('test', 'Killing pty');
-    if (ptyProcess != null) {
-        ptyProcess.kill();
-        ipcRenderer.send('test', 'Killed pty');
-    }
+currentWindow.on('close', () => {
+    instance.terminate();
 });
 
-fit(xterm);
+instance.initialize();
---
+++ app/xterm.instance.js
@@ -0,0 +1,48 @@
+const pty = require('node-pty');
+const term = require('xterm').Terminal;
+const { fit } = require('xterm/lib/addons/fit/fit');
+
+module.exports = class Term {
+
+    constructor(option, element) {
+        this._option = option;
+        this._element = element;
+        this._terminal = new term();
+
+        this._pty = pty.spawn('powershell.exe', [], {
+            name: 'xterm-color',
+            cwd: process.cwd(),
+            env: process.env,
+            cols: 80,
+            rows: 30
+        });
+
+        this._terminal.open(this._element);
+
+        this._terminal.on('data', (data) => {
+            this._pty.write(data);
+        });
+
+        this._pty.on('data', (data) => {
+            this._terminal.write(data);
+        });
+
+        this._pty.on('exit', (data) => {
+            //currentWindow.close();
+        });
+    }
+
+    initialize() {
+        if (!this._terminal) {
+            return;
+        }
+        fit(this._terminal);
+    }
+
+    terminate() {
+        if (!this._pty) {
+            return;
+        }
+        this._pty.kill();
+    }
+}
